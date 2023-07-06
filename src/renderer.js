130106646b5e929faf81da18a11ebbc248ebe3dd
xjs0n <babtisedinfire4021@gmail.com>
2023-07-06 16:55:31 +0200

    Added images, and multiple cards

--- src/renderer.js
+++ src/renderer.js
@@ -1,24 +1,73 @@
-var fs = require('fs');
+const fs = require('fs');
+const https = require('https');
 window.$ = window.jQuery = require('jquery');
 
-var settings;
+
 fs.readFile("./data/settings.json", 'utf-8', (err, data) => {
   if (err) {
     alert("An error ocurred reading the file :" + err.message);
     return;
   }
-  settings = JSON.parse(data);
+  var settings = JSON.parse(data);
 
-  $("#locations").html(
-    settings.locations.map(location => `<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="${location}">
-  <div class="card-body">
-    <p class="card-text">
-    <h4>${location}</h4>
-    <hr/>
-    Any text
-    </p>
-  </div>
-  </div>`)
-    )
+  let row = 0;
+  let count = 0;
+
+  settings.locations.forEach(location => {
+    let url = `https://api.weatherapi.com/v1/forecast.json?q=${location.replace(" ", "+")}&key=772a0b56f48149caa8b154043230507&days=3`;
+    https.get(url, (res) => {
+      let tmp = [];
+
+      res.on('data', chunk => {
+        tmp.push(chunk);
+      });
+
+      res.on('end', () => {
+        let weather_data = JSON.parse(Buffer.concat(tmp).toString());
+
+        if (count % 4 == 0) {
+          row++;
+          $("#locations").append(`<div class="row align-items-center mb-3" id="locations-row-${row}"></div>`);
+        }
+
+
+        let card = weather_data.error ?
+          `<div class="col-sm-3">
+            <div class="card">
+            <img src="https://cdn3.iconfinder.com/data/icons/smileys-people-smiley-essential/48/v-51-512.png" class="weather-icon card-img-top" alt=":(">
+              <div class="card-body">
+                <h5 class="card-title">${location}</h5>
+                <hr/>
+                <p class="card-text">This location is not currently supported.</p>
+              </div>
+            </div>
+          </div>`
+          :
+          `<div class="col-sm-3">
+            <div class="card">
+            <div class="row align-items-center">
+              <div class="col-6 align-items-center"><img src="https:${weather_data.current.condition.icon}" class="weather-icon card-img-top" alt="${location}"></div>
+              <div class="col-6 align-items-center"><p class="temp-text">${weather_data.current.temp_c}°C</p></div>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">${location}</h4>
+              <hr/>
+              <p class="card-text">${weather_data.location.country}</p>
+              <ul class="list-group list-group-flush" id="days-${count}">
+              </ul>
+              <p class="card-text"><small class="text-muted">Updated ${weather_data.current.last_updated}</small></p>
+            </div>
+            </div>
+          </div>`;
+
+        $(`#locations-row-${row}`).append(card);
+
+        $(`#days-${count}`).append();
+
+        count++;
+      });
+
+    })
+  });
 });
+
